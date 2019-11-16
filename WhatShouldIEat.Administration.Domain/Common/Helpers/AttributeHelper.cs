af9bc1816da2d13651b76ff6a48c762fa1c724b9
Dawid Pereira <pereiradawid@outlook.com>
2019-11-16 02:18:02 +0100

    Added CalorieCalculator Tesrs

--- WhatShouldIEat.Administration.Domain/Common/Helpers/AttributeHelper.cs
+++ WhatShouldIEat.Administration.Domain/Common/Helpers/AttributeHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Reflection;
+using WhatShouldIEat.Administration.Domain.Ingredient.Entities.MacroNutrient;
 
 namespace WhatShouldIEat.Administration.Domain.Common.Helpers
 {
 	public static class AttributeHelper<T>
 	{
-		public static TA GetAttribute<TA>(T attributeOwner) where TA : Attribute
-			=> Attribute.GetCustomAttribute(ForValue(attributeOwner), typeof(T)) as TA;
+		public static TA GetAttribute<TA>(T attributeOwner) where TA : MacroNutrientAttribute 
+			=> Attribute.GetCustomAttribute(ForValue(attributeOwner), typeof(TA)) as TA;
+
 		private static MemberInfo ForValue(T attributeOwner) 
 			=> typeof(T).GetField(Enum.GetName(typeof(T), attributeOwner));
 	}
---
+++ WhatShouldIEat.AdministrationService.UnitTests/CalorieCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+using WhatShouldIEat.Administration.Domain.Ingredient.Entities.MacroNutrient;
+
+namespace WhatShouldIEat.AdministrationService.Tests
+{
+	[TestFixture]
+	public class CalorieCalculatorTests
+	{
+		[Test]
+		public void CalorieCalculator_WhenCalled_ReturnProperValue()
+		{
+			var fatResult = MacroNutrient.Fat.CountCalorie(100);
+			var proteinResult = MacroNutrient.Protein.CountCalorie(100);
+			var carbohydrateResult = MacroNutrient.Carbohydrate.CountCalorie(100);
+
+			using (new AssertionScope())
+			{
+				fatResult.Should().Be(900);
+				proteinResult.Should().Be(400);
+				carbohydrateResult.Should().Be(400);
+			}
+		}
+	}
+}
