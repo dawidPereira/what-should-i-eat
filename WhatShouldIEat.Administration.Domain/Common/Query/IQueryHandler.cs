abfa7e4671c852fc12ff033f58d7be57f9747c6a
Dawid Pereira <pereiradawid@outlook.com>
2019-11-18 23:53:42 +0100

    Added Query interfaces

---
+++ WhatShouldIEat.Administration.Domain/Common/Query/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using WhatShouldIEat.Administration.Domain.Common.Query;
+
+namespace WhatShouldIEat.Administration.Domain.Common.Query
+{
+	public interface IQueryHandler<in TQuery, out TResult> where TQuery : IQuery<TResult>
+	{
+		TResult Handle(TQuery query);
+	}
+}
