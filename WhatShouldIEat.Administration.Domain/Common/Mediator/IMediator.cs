67f7321472a1c101dc0ad9bc6a930dda873e3af7
Dawid Pereira <pereiradawid@outlook.com>
2019-11-18 23:53:08 +0100

    Configured Mediator

---
+++ WhatShouldIEat.Administration.Domain/Common/Mediator/IMediator.cs
@@ -0,0 +1,16 @@
+﻿
+using WhatShouldIEat.Administration.Domain.Common.Command;
+using WhatShouldIEat.Administration.Domain.Common.Query;
+using WhatShouldIEat.Administration.Domain.ValueObjects;
+
+namespace WhatShouldIEat.Administration.Domain.Common.Mediator
+{
+	public interface IMediator
+	{
+		Result Command<TCommand>(TCommand command) where TCommand : ICommand;
+
+		TResponse Query<TResponse>(IQuery<TResponse> query);
+
+		TResponse Query<TQuery, TResponse>(TQuery query) where TQuery : IQuery<TResponse>;
+	}
+}
