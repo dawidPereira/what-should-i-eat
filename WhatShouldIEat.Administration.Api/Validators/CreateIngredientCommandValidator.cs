1517d3e5e8c783771ea8ff8712e7f56c877ced05
Dawid Pereira <pereiradawid@outlook.com>
2019-11-19 21:11:49 +0100

    Added CreateIngredientCommand Validator

---
+++ WhatShouldIEat.Administration.Api/Validators/CreateIngredientCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using WhatShouldIEat.Administration.Domain.Ingredients.Command;
+
+namespace WhatShouldIEat.Administration.Api.Validators
+{
+	public class CreateIngredientCommandValidator : AbstractValidator<CreateIngredientCommand>
+	{
+		public CreateIngredientCommandValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.").When(x => x.Allergens != null);
+			RuleFor(x => x.Allergens).NotEmpty();
+			RuleFor(x => x.Requirements).NotEmpty();
+			RuleFor(x => x.MacroNutrients).NotEmpty();
+			RuleForEach(x => x.MacroNutrients).SetValidator(new MacroNutrientTupleValidator());
+		}
+	}
+}
---
+++ WhatShouldIEat.Administration.Api/Validators/MacroNutrientTupleValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentValidation;
+using WhatShouldIEat.Administration.Domain.Ingredients.Entities.MacroNutrients;
+
+namespace WhatShouldIEat.Administration.Api.Validators
+{
+	public class MacroNutrientTupleValidator : AbstractValidator<Tuple<MacroNutrient, double>>
+	{
+		public MacroNutrientTupleValidator()
+		{
+			RuleFor(x => x.Item2).GreaterThanOrEqualTo(0);
+		}
+	}
+}
