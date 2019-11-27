ebebab73362482daabeed2a3a49aa5d3981a1551
Dawid Pereira <pereiradawid@outlook.com>
2019-11-27 22:46:25 +0100

    Configured DI Container

---
+++ WhatShouldIEat.Administration.Domain/Common/Validators/ValidatorsDiConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WhatShouldIEat.Administration.Domain.Ingredients.Commands.Create;
+
+namespace WhatShouldIEat.Administration.Domain.Common.Validators
+{
+	public static class ValidatorsDiConfiguration
+	{
+		public static IServiceCollection AddDomainValidators(this IServiceCollection services)
+		{
+			services.Scan(scan =>
+				scan
+					.FromAssemblyOf<CreateIngredientCommand>()
+					.AddClasses(classes => classes.AssignableTo(typeof(ICommandValidator<>)))
+					.AsImplementedInterfaces()
+					.WithTransientLifetime());
+			
+			return services;
+		}
+	}
+}
