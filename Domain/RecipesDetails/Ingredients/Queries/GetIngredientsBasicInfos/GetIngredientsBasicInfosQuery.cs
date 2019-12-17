430d3ffb9964fa4437fd400e68d025573de2cb07
Dawid Pereira <pereiradawid@outlook.com>
2019-12-17 21:52:38 +0100

    Removed duplicated ingredient files

--- Domain/RecipesDetails/Ingredients/Queries/GetIngredientsBasicInfos/GetIngredientsBasicInfosQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Domain.Common.Mediators.Queries;
-
-namespace Domain.RecipesDetails.Ingredients.Queries.GetIngredientsBasicInfos
-{
-	public class GetIngredientsBasicInfosQuery : IQuery<ICollection<IngredientBasicInfo>>
-	{
-	}
-}
