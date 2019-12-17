75a4d5b7aab30f8f880e65ecaaf7fb3221efe6e1
Dawid Pereira <pereiradawid@outlook.com>
2019-12-17 19:30:55 +0100

    Added Ingredient Created Event

---
+++ Domain/Ingredients/Events/IngredientCreatedEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Domain.Common.Mediators.Events;
+
+namespace Domain.Ingredients.Events
+{
+	public class IngredientCreatedEvent : IEvent<IngredientCreatedEvent>
+	{
+		public IngredientCreatedEvent(Guid ingredientId, string queueName)
+		{
+			EventIdentity = new EventIdentity(queueName);
+			IngredientId = ingredientId;
+		}
+
+		public Guid IngredientId { get; }
+		public IEventIdentity EventIdentity { get; }
+
+		public bool Equals(IngredientCreatedEvent other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return Equals(EventIdentity, other.EventIdentity) && IngredientId.Equals(other.IngredientId);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			return obj.GetType() == GetType() && Equals((IngredientCreatedEvent) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return ((EventIdentity != null ? EventIdentity.GetHashCode() : 0) * 397) ^ IngredientId.GetHashCode();
+			}
+		}
+	}
+}
