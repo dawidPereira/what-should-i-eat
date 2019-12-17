2fd76808653b5fc3b7143e0044bac42a953dd289
Dawid Pereira <pereiradawid@outlook.com>
2019-12-17 20:03:48 +0100

    Added IngredientTestFactory

---
+++ UnitTests/Ingredients/Factories/IngredientFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Common.Mediators.Events;
+using Domain.Ingredients.Entities;
+using Domain.Ingredients.Entities.MacroNutrients;
+
+namespace WhatShouldIEat.AdministrationService.Tests.Ingredients.Factories
+{
+	internal static class IngredientFactory
+	{
+		private const Allergen Allergens = Allergen.Gluten | Allergen.Milk;
+		private const Requirement Requirements = Requirement.Ecological | Requirement.ForVegan;
+		
+		internal static Ingredient CreateIngredientWithOneShare(string name, MacroNutrient macroNutrient, IEventPublisher eventPublisher)
+		{
+			var shares = new HashSet<MacroNutrientShare>{new MacroNutrientShare(macroNutrient, 0.2)};
+			var macroNutrientsShares = new MacroNutrientsSharesCollection(shares);
+			return new Ingredient(Guid.NewGuid(), name, Allergens, Requirements, macroNutrientsShares, eventPublisher);
+		}
+		
+		internal static Ingredient CreateValidIngredient(string name, IEventPublisher eventPublisher)
+		{
+			var shares = new HashSet<MacroNutrientShare>
+			{
+				new MacroNutrientShare(MacroNutrient.Carbohydrate, 0.2),
+				new MacroNutrientShare(MacroNutrient.Fat, 0.2),
+				new MacroNutrientShare(MacroNutrient.Protein, 0.3)
+			};
+			var macroNutrientsShares = new MacroNutrientsSharesCollection(shares);
+			return new Ingredient(Guid.NewGuid(), name, Allergens, Requirements, macroNutrientsShares, eventPublisher);
+		}
+		
+		internal static Ingredient CreateIngredientWithInvalidId(string name, IEventPublisher eventPublisher)
+		{
+			var shares = new HashSet<MacroNutrientShare>{new MacroNutrientShare(MacroNutrient.Carbohydrate, 0.2)};
+			var macroNutrientsShares = new MacroNutrientsSharesCollection(shares);
+			return new Ingredient(new Guid(""), name, Allergens, Requirements, macroNutrientsShares, eventPublisher);
+		}
+	}
+}
