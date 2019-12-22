fae53b673680c7fba876054144cdf6cf3d9399ba
Dawid Pereira <pereiradawid@outlook.com>
2019-12-22 21:00:39 +0100

    Added RecipeIngredientTests

---
+++ UnitTests/Recipes/Entities/RecipeIngredientTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Domain.Common.ValueObjects;
+using Domain.Ingredients.Repositories;
+using Domain.Recipes.Entities;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace WhatShouldIEat.AdministrationService.Tests.Recipes.Entities
+{
+	[TestFixture]
+	internal class RecipeIngredientTest
+	{
+		private Mock<IIngredientRepository> _ingredientRepositoryMock;
+		private RecipeIngredient.RecipeIngredientFactory _recipeIngredientFactory;
+		
+		[SetUp]
+		public void SetUp()
+		{
+			_ingredientRepositoryMock.Setup(x => x.ExistById(It.IsAny<Identity<Guid>>()))
+				.Returns(true);
+			_ingredientRepositoryMock = new Mock<IIngredientRepository>();
+			_recipeIngredientFactory = new RecipeIngredient.RecipeIngredientFactory(_ingredientRepositoryMock.Object);
+		}
+
+		[Test]
+		public void GivenNegativeGrams_WhenCreatingRecipeIngredient_ThrowArgumentException()
+		{
+			Action action = () => _recipeIngredientFactory.Create(new Identity<Guid>(Guid.NewGuid()), -200);
+			action.Should().Throw<ArgumentException>();
+		}
+		
+		[Test]
+		public void GivenNotExistedIngredientId_WhenCreatingRecipeIngredient_ThrowArgumentException()
+		{
+			_ingredientRepositoryMock.Setup(x => x.ExistById(It.IsAny<Identity<Guid>>()))
+				.Returns(false);
+			Action action = () => _recipeIngredientFactory.Create(new Identity<Guid>(Guid.NewGuid()), 200);
+			action.Should().Throw<ArgumentException>();
+		}
+	}
+}
